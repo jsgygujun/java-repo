dfa21bead5792ad5d47efb0e591c537ff14ca945
jsgygujun <jsgygujun@gmail.com>
2020-12-17 16:24:52 +0800

    [ADD] OOM demo.

---
+++ src/main/java/com/java/repo/code/oom_error/StackOverflowOOMDemo.java
@@ -0,0 +1,20 @@
+package com.java.repo.code.oom_error;
+
+// -Xss[1024k]
+public class StackOverflowOOMDemo {
+
+    static void stackOverflowOOM(int i) {
+        ++i;
+        System.out.println("第[" + i + "]层～");
+        stackOverflowOOM(i);
+    }
+
+    public static void main(String[] args) {
+        int i = 0;
+        try {
+            stackOverflowOOM(i);
+        } catch (Error e) {
+            e.printStackTrace(); // Exception in thread "main" java.lang.StackOverflowError
+        }
+    }
+}
