073773e93863d81edf9dc2dac1f85ffae977b85c
jsgygujun <jsgygujun@gmail.com>
2020-12-21 20:55:17 +0800

    [ADD] 线程相关。

---
+++ src/main/java/com/java/repo/code/thread/MainThreadDemo.java
@@ -0,0 +1,54 @@
+package com.java.repo.code.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 1. 主线程退出，子线程（非守护线程）不会退出
+ * 2. 主线程退出，子线程（守护线程）退出
+ * 3. 创建一个线程池（非守护线程），主线程退出，JVM不会退出
+ * @author GuJun
+ * @date 2020/12/21
+ */
+public class MainThreadDemo {
+
+    private static void mainThreadExit() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+                System.out.println(Thread.currentThread().getName() + " exit~~");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "subThread").start();
+        System.out.println("mainThread exit~~~");
+    }
+
+    private static void mainThreadExitDaemonThread() {
+        Thread daemonThread = new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+                System.out.println(Thread.currentThread().getName() + " exit~~");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "daemonSubThread");
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+        System.out.println("mainThread exit~~~");
+    }
+
+    private static void mainThreadExecutors() {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(() -> {
+            System.out.println(Thread.currentThread().getName() + "doing job~");
+        });
+        System.out.println("mainThread exit~~~");
+    }
+
+    public static void main(String[] args) {
+        //mainThreadExit();
+        //mainThreadExitDaemonThread();
+        mainThreadExecutors();
+    }
+}
