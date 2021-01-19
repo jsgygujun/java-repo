9a67c7fe13ece94378dbfe695006081a8a5d97e3
jun <jsgygujun@gmail.com>
2021-01-19 18:42:03 +0800

    [ADD] 常量池

---
+++ src/main/java/com/java/repo/code/constant_pool/Student.java
@@ -0,0 +1,25 @@
+package com.java.repo.code.constant_pool;
+
+import lombok.Data;
+
+/**
+ * @author GuJun
+ * @date 2021/1/19
+ */
+@Data
+public class Student {
+    Integer age;
+    String name;
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setAge(333);
+        s1.setName("张三");
+
+        Student s2 = new Student();
+        s2.setAge(333);
+        s1.setName("李四");
+
+        System.out.println(s1.getAge() == s2.getAge()); // false
+    }
+}
