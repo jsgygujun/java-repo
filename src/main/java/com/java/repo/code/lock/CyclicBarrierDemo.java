c99459f67ae618968970244d3706ff16485e8dcf
jun <jsgygujun@gmail.com>
2021-01-21 17:47:13 +0800

    [UPDATE] 锁的辅助类

---
+++ src/main/java/com/java/repo/code/lock/CyclicBarrierDemo.java
@@ -0,0 +1,27 @@
+package com.java.repo.code.lock;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @author GuJun
+ * @date 2021/1/21
+ */
+public class CyclicBarrierDemo {
+    public static void main(String[] args) {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
+            System.out.println("召唤神龙～～");
+        });
+        for (int i = 1; i <= 7; ++i) {
+            final int finalI = i;
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName() + "收集到第「" + finalI + "」颗龙珠～～");
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }, "Thread---"+i).start();
+        }
+    }
+}
